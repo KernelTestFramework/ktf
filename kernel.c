f3d992b516a2e31884aed40931a18e4263d90751
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-03 12:28:29 +0000

    string: add string handling functionality
    Add strtoul(), strtol() and vsnprintf() implementation from FreeBSD.
    Implement basic ctypes.h helpers (isspace, isdigit, etc).
    
    Move the simple strlen() to string.h header file.
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

--- kernel.c
+++ kernel.c
@@ -1,4 +1,5 @@
 #include <ktf.h>
+#include <string.h>
 
 static void putc(int c) {
     asm volatile("outb %%al, %%dx"
@@ -10,16 +11,6 @@ static void com1_write(const char *buf, size_t len) {
     asm volatile("rep; outsb"
                  : "+S" (buf), "+c" (len)
                  : "d" (0x3f8));
-}
-
-
-static size_t strlen(const char *str) {
-    size_t len = 0;
-
-    while (str[len])
-        len++;
-
-    return len;
 }
 
 void kernel_start(void) {
