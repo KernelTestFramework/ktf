7a855569a16d26f34269cb64a90d5794c4eb2c67
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-03 12:39:03 +0000

    multiboot: display basic multiboot data
    Display input cmdline content on the console.
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

--- kernel.c
+++ kernel.c
@@ -1,9 +1,11 @@
 #include <ktf.h>
 #include <console.h>
 #include <string.h>
+#include <multiboot.h>
 
-void kernel_start(void) {
+void kernel_start(multiboot_info_t *mbi, const char *cmdline) {
     register_console_callback(serial_console_write);
 
     printk("KTF - KVM Test Framework!\n");
+    printk("Multiboot cmdline: %s\n", cmdline);
 }
