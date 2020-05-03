5129e2548d7ebc6cf068273c23a39363dac10fcb
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-03 12:18:58 +0000

    arch: add long mode support
    - processor.h:
      Add x86 CPU definitions for flags, control registers and some MSRs.
    
    Add CS64 descriptor to GDT and define CS64 selector.
    Enable PAE in CR4 and set pagetables in CR3. Set EFER_LME in EFER MSR
    and then activate long mode by enabling paging.
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

--- desc.c
+++ desc.c
@@ -6,6 +6,7 @@ gdtdesc_t gdt[] __aligned(16) = {
 	[GDT_NULL]      = _U64(0),
 	[GDT_KERN_CS32] = GDT_ENTRY(0xcf9b, 0, 0xfffff), /* Flags: Gr=1, Sz=1; Access Byte: P=1, DPL=0, S=1, E=1, RW=1, A=1 */
 	[GDT_KERN_DS32] = GDT_ENTRY(0xcf93, 0, 0xfffff), /* Flags: Gr=1, Sz=1; Access Byte: P=1, DPL=0, S=1, E=0, RW=1, A=1 */
+	[GDT_KERN_CS64] = GDT_ENTRY(0xa09b, 0, 0x00000), /* Flags: Gr=1,  L=1; Access Byte: P=1, DPL=0, S=1, E=1, RW=1, A=1 */
 };
 
 gdt_ptr_t gdt_ptr __section(".data") = {
