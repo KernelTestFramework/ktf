2a2dee265426e6ae8b5760b9d2f47661c039e733
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-03 11:30:43 +0000

    arch: export halt loop as arch_crash()
    Make arch_crash() global and use pause.
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

--- include/asm-macros.h
+++ include/asm-macros.h
@@ -17,6 +17,13 @@ name:
     .section name, flags;               \
     .align alignment
 
+#define SIZE(name) \
+    .size name, (. - name);
+
+#define END_FUNC(name) \
+    .type name, STT_FUNC;   \
+    SIZE(name)
+
 #endif
 
 #endif /* KTF_ASM_MACROS_H */
