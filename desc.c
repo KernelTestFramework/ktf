bc8e7c493634dddbbf0abc8b41c6438a7e304c21
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-03 11:42:05 +0000

    arch: add dedicated files for segment descriptors
    - desc.c:
      Declare global GDT table and gdt_ptr pointer to it with NULL, CS32
      and DS32 descriptors.

---
+++ desc.c
@@ -0,0 +1,15 @@
+#include <ktf.h>
+#include <asm-macros.h>
+#include <desc.h>
+
+gdtdesc_t gdt[] __aligned(16) = {
+	[GDT_NULL]      = _U64(0),
+	[GDT_KERN_CS32] = GDT_ENTRY(0xcf9b, 0, 0xfffff), /* Flags: Gr=1, Sz=1; Access Byte: P=1, DPL=0, S=1, E=1, RW=1, A=1 */
+	[GDT_KERN_DS32] = GDT_ENTRY(0xcf93, 0, 0xfffff), /* Flags: Gr=1, Sz=1; Access Byte: P=1, DPL=0, S=1, E=0, RW=1, A=1 */
+};
+
+gdt_ptr_t gdt_ptr __section(".data") = {
+	.size = sizeof(gdt) - 1,
+	.addr = _ul(&gdt[0]),
+};
+
---
+++ include/desc.h
@@ -0,0 +1,30 @@
+#ifndef KTF_DESC_H
+#define KTF_DESC_H
+
+#define GDT_NULL      0x0
+#define GDT_KERN_CS32 0x1
+#define GDT_KERN_DS32 0x2
+
+#define __KERN_CS32 (GDT_KERN_CS32 << 3)
+#define __KERN_DS32 (GDT_KERN_DS32 << 3)
+
+#define GDT_ENTRY(flags, base, limit)                \
+        ((((base)  & _U64(0xff000000)) << (56-24)) | \
+         (((flags) & _U64(0x0000f0ff)) << 40)      | \
+         (((limit) & _U64(0x000f0000)) << (48-16)) | \
+         (((base)  & _U64(0x00ffffff)) << 16)      | \
+         (((limit) & _U64(0x0000ffff))))
+
+#ifndef __ASSEMBLY__
+
+typedef uint64_t gdtdesc_t;
+
+struct gdt_ptr {
+        uint16_t size;
+        uint64_t addr;
+} __packed;
+typedef struct gdt_ptr gdt_ptr_t;
+
+#endif
+
+#endif /* KTF_DESC_H */
