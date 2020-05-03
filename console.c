3dbbfbf728b8ccd1d88a691ccd087710db4c422d
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-03 12:38:14 +0000

    console: add multi console support
    Currently two console types are supported: QEMU and Serial console.
    Add console output callback support similar to the one in XTF.
    Allow kernel to register desired console output via
    register_console_callback().
    
    Implement global printk() based on static vprintk(), that uses vsnprintf()
    and outputs resulted buffer content to all registered consoles.
    The vprintk() uses a static buffer of 1024 bytes for simplicity
    (it's not thread-safe).
    
    Implement panic() dumping an error message and calling arch_crash().
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

---
+++ console.c
@@ -0,0 +1,77 @@
+#include <ktf.h>
+#include <string.h>
+#include <console.h>
+
+#define QEMU_CONSOLE   0x0e9
+#define SERIAL_CONSOLE 0x3f8
+
+#define VPRINTK_BUF_SIZE 1024
+
+static console_callback_t console_callbacks[2];
+static unsigned int num_console_callbacks;
+
+static inline void putc(int c, int port) {
+    asm volatile("outb %%al, %%dx"
+                 : "+a" (c)
+                 : "d" (port));
+}
+
+static inline void puts(const char *buf, size_t len, int port) {
+    asm volatile("rep; outsb"
+                 : "+S" (buf), "+c" (len)
+                 : "d" (port));
+}
+
+static void vprintk(const char *fmt, va_list args) {
+    static char buf[VPRINTK_BUF_SIZE];
+    unsigned int i;
+    int rc;
+
+    rc = vsnprintf(buf, sizeof(buf), fmt, args);
+
+    if ( rc > (int)sizeof(buf) )
+        panic("vprintk() buffer overflow\n");
+
+    for ( i = 0; i < num_console_callbacks; i++ )
+        console_callbacks[i](buf, rc);
+}
+
+void printk(const char *fmt, ...) {
+    va_list args;
+
+    va_start(args, fmt);
+    vprintk(fmt, args);
+    va_end(args);
+}
+
+void putchar(int c) {
+    putc(c, SERIAL_CONSOLE);
+}
+
+void serial_console_write(const char *buf, size_t len) {
+    puts(buf, len, SERIAL_CONSOLE);
+}
+
+void qemu_console_write(const char *buf, size_t len) {
+    puts(buf, len, QEMU_CONSOLE);
+}
+
+void register_console_callback(console_callback_t cb) {
+    console_callbacks[num_console_callbacks++] = cb;
+}
+
+void __noreturn panic(const char *fmt, ...) {
+    va_list args;
+
+    printk("******************************\n");
+    printk("PANIC: ");
+
+    va_start(args, fmt);
+    vprintk(fmt, args);
+    va_end(args);
+
+    printk("******************************\n");
+
+    while(1)
+        arch_crash();
+}
