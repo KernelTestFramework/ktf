dd7d67d0fbfb57ec22e7dd85e5fa89ceade91ecb
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-03 11:26:35 +0000

    include: add global, common ktf.h and compiler.h header files
    - ktf.h:
      It provides stdinc headers and global, common prototypes declarations.
    
    - compiler.h:
      It provides common compilation utilities, simplifications and
      definitions.
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

---
+++ include/ktf.h
@@ -0,0 +1,15 @@
+#ifndef KTF_KTF_H
+#define KTF_KTF_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <inttypes.h>
+#include <limits.h>
+
+#include <compiler.h>
+
+extern void arch_crash(void);
+
+#endif /* KTF_KTF_H */
