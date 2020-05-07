457c46990592785f299893ce3746c66da1bc1857
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-07 08:48:45 +0000

    arch: add IDT and exceptions support
    Declare and register handlers for typical x86 exceptions.
    Align stack for all exception types (with and without error code) by
    pushing zero when there is no error code.
    Store vector number in upper 32bit of error code on the stack.
    
    Upon exception, jump to handle_exception() routine which preserves
    register context and jumps to do_exception(), which in turn panics.
    The do_exception() receives a pointer to saved register context on the
    stack.
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

--- desc.c
+++ desc.c
@@ -1,5 +1,6 @@
 #include <ktf.h>
 #include <asm-macros.h>
+#include <processor.h>
 #include <desc.h>
 
 gdtdesc_t gdt[] __aligned(16) = {
@@ -14,3 +15,32 @@ gdt_ptr_t gdt_ptr __section(".data") = {
     .addr = _ul(&gdt),
 };
 
+idt_entry_t idt[256] = {
+   [X86_EX_DE]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_DB]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_NMI] = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_BP]  = INTR_GATE(__KERN_CS, _ul(0), DPL3, PRESENT),
+   [X86_EX_OF]  = INTR_GATE(__KERN_CS, _ul(0), DPL3, PRESENT),
+   [X86_EX_BR]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_UD]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_NM]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_DF]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_CS]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_TS]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_NP]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_SS]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_GP]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_PF]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_SPV] = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_MF]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_AC]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_MC]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_XM]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_VE]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+   [X86_EX_SE]  = INTR_GATE(__KERN_CS, _ul(0), DPL0, PRESENT),
+};
+
+idt_ptr_t idt_ptr __section(".data") = {
+    .size = sizeof(idt) - 1,
+    .addr = _ul(&idt),
+};
--- include/desc.h
+++ include/desc.h
@@ -11,6 +11,14 @@
 #define __KERN_DS32 (GDT_KERN_DS32 << 3)
 #define __KERN_CS64 (GDT_KERN_CS64 << 3)
 
+#if defined(__i386__)
+#define __KERN_CS __KERN_CS32
+#define __KERN_DS __KERN_DS32
+#else
+#define __KERN_CS __KERN_CS64
+#define __KERN_DS (0)
+#endif
+
 #define GDT_ENTRY(flags, base, limit)  { .quad =     \
         ((((base)  & _U64(0xff000000)) << (56-24)) | \
          (((flags) & _U64(0x0000f0ff)) << 40)      | \
@@ -67,12 +75,125 @@ typedef struct x86_segment_desc x86_segment_desc_t;
 
 typedef x86_segment_desc_t gdtdesc_t;
 
-struct gdt_ptr {
-        uint16_t size;
-        uint64_t addr;
+struct desc_ptr {
+    uint16_t size;
+#if defined(__i386__)
+    uint32_t addr;
+#else
+    uint64_t addr;
+#endif
 } __packed;
-typedef struct gdt_ptr gdt_ptr_t;
 
+typedef struct desc_ptr gdt_ptr_t;
+typedef struct desc_ptr idt_ptr_t;
+
+/*
+ * Protected mode IDT entry, GDT task/call gate (8-byte)
+ */
+struct __packed x86_gate32 {
+    union {
+        struct {
+            uint32_t lo, hi;
+        };
+        struct {
+            uint16_t offset_lo;
+            uint16_t selector;
+            uint8_t rsvd;
+            unsigned int type:4, s:1, dpl:2, p:1;
+            uint16_t offset_hi;
+        };
+    };
+};
+
+static inline void set_gate32_offset(struct x86_gate32 *gate, unsigned long offset) {
+    gate->offset_lo = (offset & _ul(0x0000ffff));
+    gate->offset_hi = ((offset & _ul(0xffff0000)) >> 16);
+}
+
+/*
+ * Long mode IDT entry, GDT call gate (16-byte)
+ */
+struct __packed x86_gate64 {
+    union {
+        struct {
+            uint64_t lo, hi;
+        };
+        struct {
+            uint16_t offset_lo;
+            uint16_t selector;
+            unsigned int ist:3, rsvd0:5;
+            unsigned int type:4, s:1, dpl:2, p:1;
+            uint16_t offset_mi;
+            uint32_t offset_hi;
+            uint32_t rsvd1;
+        };
+    };
+};
+
+static inline void set_gate64_offset(struct x86_gate64 *gate, unsigned long offset) {
+    gate->offset_lo =  (offset & _ul(0x000000000000ffff));
+    gate->offset_mi = ((offset & _ul(0x00000000ffff0000)) >> 16);
+    gate->offset_hi = ((offset & _ul(0xffffffff00000000)) >> 32);
+}
+
+#define GATE_NOT_PRESENT 0x00
+#define GATE_PRESENT 0x01
+#define GATE_DPL0 0x00
+#define GATE_DPL3 0x03
+
+#define GATE_TYPE_INTR _U32(0xE)
+#define GATE_TYPE_TRAP _U32(0xF)
+#define GATE_TYPE_TASK _U32(0x5)
+
+#define _GATE_ENTRY32(type, selector, offset, dpl, present) { \
+    .offset_lo = ((offset) & _U32(0x0000ffff)),               \
+    .offset_hi = (((offset) & _U32(0xffff0000)) >> 16),       \
+    .selector =  ((selector) & _U32(0xffff)),                 \
+    .rsvd = 0, .s = 0, .type = ((type) & _U32(0xf)),          \
+    .dpl = ((dpl) & _U32(0x3)), .p = ((present) & 0x1) }
+
+#define INTR_GATE32(selector, offset, dpl, present) _GATE_ENTRY32(GATE_TYPE_INTR, selector, offset, dpl, present)
+#define TRAP_GATE32(selector, offset, dpl, present) _GATE_ENTRY32(GATE_TYPE_TRAP, selector, offset, dpl, present)
+#define TASK_GATE32(selector, dpl, present) _GATE_ENTRY32(GATE_TYPE_TASK, selector, _U32(0x0), dpl, present)
+
+#define _GATE_ENTRY64(_type, _selector, _offset, _dpl, _present) { \
+    .offset_lo = (((_offset) & _U64(0x000000000000ffff))),         \
+    .offset_mi = (((_offset) & _U64(0x00000000ffff0000)) >> 16),   \
+    .offset_hi = (((_offset) & _U64(0xffffffff00000000)) >> 32),   \
+    .selector = ((_selector) & _U32(0xffff)),                      \
+    .rsvd0 = 0, .rsvd1 = 0, .s = 0, .type = ((_type) & _U32(0xf)), \
+    .dpl = ((_dpl) & _U32(0x3)), .p = ((_present) & 0x1) }
+
+#define INTR_GATE64(selector, offset, dpl, present) _GATE_ENTRY64(GATE_TYPE_INTR, selector, offset, dpl, present)
+#define TRAP_GATE64(selector, offset, dpl, present) _GATE_ENTRY64(GATE_TYPE_TRAPf, selector, offset, dpl, present)
+
+#if defined(__i386__)
+
+typedef struct x86_gate32 task_gate_t;
+typedef struct x86_gate32 idt_entry_t;
+
+#define INTR_GATE(selector, offset, dpl, present) INTR_GATE32(selector, offset, GATE_ ## dpl, GATE_ ## present)
+#define TRAP_GATE(selector, offset, dpl, present) TRAP_FATE32(selector, offset, GATE_ ## dpl, GATE_ ## present)
+#define TASK_GATE(selector, dpl, present) TASK_GATE32(selector, GATE_ ## dpl, GATE_ ## present)
+
+#define set_gate_offset(gate, offset) set_gate32_offset((gate), (offset))
+
+#else
+
+typedef struct x86_gate64 task_gate_t;
+typedef struct x86_gate64 idt_entry_t;
+
+#define INTR_GATE(selector, offset, dpl, present) INTR_GATE64(selector, offset, GATE_ ## dpl, GATE_ ## present)
+#define TRAP_GATE(selector, offset, dpl, present) TRAP_FATE64(selector, offset, GATE_ ## dpl, GATE_ ## present)
+
+#define set_gate_offset(gate, offset) set_gate64_offset((gate), (offset))
+
+#endif
+
+extern gdtdesc_t gdt[];
+
+extern idt_entry_t idt[256];
+extern idt_ptr_t idt_ptr;
 #endif
 
 #endif /* KTF_DESC_H */
