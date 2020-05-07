e846c23c8e5ae60c48786519c096da6772814d13
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-07 08:08:47 +0000

    setup: Add arch_setup() to initialize the kernel
    As first step register console output callbacks of serial console.

--- setup.c
+++ setup.c
@@ -1,5 +1,6 @@
 #include <ktf.h>
 #include <page.h>
+#include <console.h>
 
 /*
  * KTF Stack layout:
@@ -9,3 +10,13 @@
  * kernel_stack[page 1] Regular stack
  */
 uint8_t kernel_stack[3 * PAGE_SIZE] __aligned(PAGE_SIZE) __data;
+
+void arch_setup(void) {
+    register_console_callback(serial_console_write);
+
+    /* TODO: Exception tables */
+
+    /* TODO PerCPU support */
+
+    /* TODO: SMP support */
+}
