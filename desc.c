2d1d1fd847734abb7eabfc31da68145a9a061779
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-07 07:57:16 +0000

    arch: Add simpler segment descriptor flags handling
    Add proper structure definition to easier access various fields from
    segment descriptors.
    Also define a set of uniformly named segment descriptor flags to be
    used by a combining macro.
    The macro TOKEN_OP() takes all specified fields, adds given common
    prefix token to them and creates a combined token with '|' as delimiter.
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

--- desc.c
+++ desc.c
@@ -3,14 +3,14 @@
 #include <desc.h>
 
 gdtdesc_t gdt[] __aligned(16) = {
-	[GDT_NULL]      = _U64(0),
-	[GDT_KERN_CS32] = GDT_ENTRY(0xcf9b, 0, 0xfffff), /* Flags: Gr=1, Sz=1; Access Byte: P=1, DPL=0, S=1, E=1, RW=1, A=1 */
-	[GDT_KERN_DS32] = GDT_ENTRY(0xcf93, 0, 0xfffff), /* Flags: Gr=1, Sz=1; Access Byte: P=1, DPL=0, S=1, E=0, RW=1, A=1 */
-	[GDT_KERN_CS64] = GDT_ENTRY(0xa09b, 0, 0x00000), /* Flags: Gr=1,  L=1; Access Byte: P=1, DPL=0, S=1, E=1, RW=1, A=1 */
+    [GDT_NULL]      = GDT_ENTRY(0x0, 0x0, 0x0),
+    [GDT_KERN_CS32] = GDT_ENTRY(DESC_FLAGS(GR, SZ, P, DPL0, S, CODE, R, A), 0x0, 0xfffff),
+    [GDT_KERN_DS32] = GDT_ENTRY(DESC_FLAGS(GR, SZ, P, DPL0, S, DATA, W, A), 0x0, 0xfffff),
+    [GDT_KERN_CS64] = GDT_ENTRY(DESC_FLAGS(GR,  L, P, DPL0, S, CODE, R, A), 0x0, 0x00000),
 };
 
 gdt_ptr_t gdt_ptr __section(".data") = {
-	.size = sizeof(gdt) - 1,
-	.addr = _ul(&gdt[0]),
+    .size = sizeof(gdt) - 1,
+    .addr = _ul(&gdt),
 };
 
