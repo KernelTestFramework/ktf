f42cd0dcce536e65f15d411d4e5a8509f38a743f
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-13 11:41:53 +0000

    link: add *.init sections and start/end symbols for all sections
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

--- include/setup.h
+++ include/setup.h
@@ -13,6 +13,15 @@ extern void arch_setup(void);
 #define GET_KERN_EX_STACK() (&kernel_stack[KERN_STACK_SIZE + PAGE_SIZE])
 #define GET_KERN_EM_STACK() (&kernel_stack[KERN_STACK_SIZE + PAGE_SIZE])
 
+extern unsigned long __start_text[], __end_text[];
+extern unsigned long __start_data[], __end_data[];
+extern unsigned long __start_bss[],  __end_bss[];
+extern unsigned long __start_rodata[], __end_rodata[];
+
+extern unsigned long __start_text_init[], __end_text_init[];
+extern unsigned long __start_data_init[], __end_data_init[];
+extern unsigned long __start_bss_init[], __end_bss_init[];
+
 #endif /* __ASSEMBLY__ */
 
 #endif /* SETUP_SETUP_H */
