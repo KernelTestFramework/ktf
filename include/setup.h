2e4252c404b488c74d3b2652a00f571827d7e342
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-13 10:43:32 +0000

    setup: extend kernel stack to 3 pages
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

--- include/setup.h
+++ include/setup.h
@@ -1,15 +1,18 @@
 #ifndef SETUP_SETUP_H
 #define SETUP_SETUP_H
 
-#include <ktf.h>
+#define KERN_STACK_SIZE (3 * PAGE_SIZE)
+
+#ifndef __ASSEMBLY__
 #include <page.h>
 
+extern uint8_t kernel_stack[KERN_STACK_SIZE + 2 * PAGE_SIZE];
 extern void arch_setup(void);
 
-extern uint8_t kernel_stack[3 * PAGE_SIZE];
+#define GET_KERN_STACK()    (&kernel_stack[KERN_STACK_SIZE])
+#define GET_KERN_EX_STACK() (&kernel_stack[KERN_STACK_SIZE + PAGE_SIZE])
+#define GET_KERN_EM_STACK() (&kernel_stack[KERN_STACK_SIZE + PAGE_SIZE])
 
-#define GET_KERN_STACK()    (&kernel_stack[1 * PAGE_SIZE])
-#define GET_KERN_EX_STACK() (&kernel_stack[2 * PAGE_SIZE])
-#define GET_KERN_EM_STACK() (&kernel_stack[3 * PAGE_SIZE])
+#endif /* __ASSEMBLY__ */
 
 #endif /* SETUP_SETUP_H */
