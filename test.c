9bb99f587e64c53220465fe8c0b83198733e6950
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-13 13:26:50 +0000

    test: add basic test framework
    Add test.c where test_main() is defined.
    As a first test call into usermode and back.
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <ktf.h>
+#include <console.h>
+
+static void __user_text func(void *arg) {
+}
+
+void test_main(void) {
+    printk("\nTest:\n");
+
+    usermode_call(func, NULL);
+    printk("Test done\n");
+
+}
