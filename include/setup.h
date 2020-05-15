ab453796a705202c775f404581a8eef2ec915413
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-15 13:00:57 +0200

    setup: add COM ports I/O ports detection
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

--- include/setup.h
+++ include/setup.h
@@ -1,10 +1,15 @@
 #ifndef SETUP_SETUP_H
 #define SETUP_SETUP_H
 
+#define BDA_COM_PORTS_ENTRY 0x400
+
 #define KERN_STACK_SIZE (3 * PAGE_SIZE)
 
 #ifndef __ASSEMBLY__
 #include <page.h>
+#include <string.h>
+
+extern io_port_t com_ports[2];
 
 extern uint8_t kernel_stack[KERN_STACK_SIZE + 2 * PAGE_SIZE];
 extern uint8_t user_stack[PAGE_SIZE];
@@ -46,6 +51,16 @@ extern addr_range_t init_addr_ranges[];
 
 extern void display_memory_map(void);
 extern void dump_pagetables(void);
+
+static inline void get_com_ports(void) {
+    memcpy((void *) com_ports, (void *)(BDA_COM_PORTS_ENTRY), sizeof(com_ports));
+
+    if (com_ports[0] == 0x0)
+        com_ports[0] = 0x3f8;
+
+    if (com_ports[1] == 0x0)
+        com_ports[1] = 0x2f8;
+}
 
 #endif /* __ASSEMBLY__ */
 
