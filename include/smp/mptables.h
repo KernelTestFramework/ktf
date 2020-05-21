d7f9d75eb55bc29a49ee2a42f4a9aeb0bb7c547a
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-21 13:34:24 +0200

    smp: add MP Configuration Table support
    Find, parse, process and dump content on MP Configuration Table.
    Get all CPUs, BUSes, IOAPICs and Interrupts configurartions.
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

--- include/smp/mptables.h
+++ include/smp/mptables.h
@@ -18,6 +18,109 @@ struct mpf {
 } __packed;
 typedef struct mpf mpf_t;
 
+#define MPC_SIGNATURE (('P' << 24) | ('M' << 16) | ('C' << 8) | ('P'))
+
+enum mpc_entry_type {
+    MPC_PROCESSOR_ENTRY,
+    MPC_BUS_ENTRY,
+    MPC_IOAPIC_ENTRY,
+    MPC_IO_INT_ENTRY,
+    MPC_LOCAL_INT_ENTRY,
+};
+typedef enum mpc_entry_type mpc_entry_type_t;
+
+struct mpc_hdr {
+    uint32_t signature;
+    uint16_t length;
+    uint8_t spec_rev;
+    uint8_t checksum;
+    const char oem_id[8];
+    const char product_id[12];
+    uint32_t oem_tlb_ptr;
+    uint16_t oem_tlb_size;
+    uint16_t entry_count;
+    uint32_t lapic_base;
+    uint16_t ext_length;
+    uint8_t ext_checksum;
+    uint8_t rsvd;
+} __packed;
+typedef struct mpc_hdr mpc_hdr_t;
+
+struct mpc_processor_entry {
+   uint8_t type;
+   uint8_t lapic_id;
+   uint8_t lapic_version;
+   struct {
+       uint8_t en:1, bsp:1, rsvd0:6;
+   };
+   struct {
+       uint32_t stepping:4, model:4, family:4, rsvd1:20;
+   };
+   uint32_t feature_flags;
+   uint32_t rsvd2;
+   uint32_t rsvd3;
+} __packed;
+typedef struct mpc_processor_entry mpc_processor_entry_t;
+
+struct mpc_bus_entry {
+   uint8_t type;
+   uint8_t id;
+   uint8_t type_str[6];
+} __packed;
+typedef struct mpc_bus_entry mpc_bus_entry_t;
+
+struct mpc_ioapic_entry {
+   uint8_t type;
+   uint8_t id;
+   uint8_t version;
+   struct {
+       uint8_t en:1, rsvd:7;
+   };
+   uint32_t base_addr;
+} __packed;
+typedef struct mpc_ioapic_entry mpc_ioapic_entry_t;
+
+#define MPC_IOINT_INT 0
+#define MPC_IOINT_NMI 1
+#define MPC_IOINT_SMI 2
+#define MPC_IOINT_EXTINT 3
+
+#define MPC_IOINT_POLARITY_BS 0x00
+#define MPC_IOINT_POLARITY_AH 0x01
+#define MPC_IOINT_POLARITY_RSVD 0x10
+#define MPC_IOINT_POLARITY_AL 0x11
+
+#define MPC_IOINT_TRIGGER_BS 0x00
+#define MPC_IOINT_TRIGGER_ET 0x01
+#define MPC_IOINT_TRIGGER_RSVD 0x10
+#define MPC_IOINT_TRIGGER_LT 0x11
+
+struct mpc_ioint_entry {
+   uint8_t type;
+   uint8_t int_type;
+   struct {
+       uint16_t po:1, el:1, rsvd:14;
+   };
+   uint8_t src_bus_id;
+   uint8_t src_bus_irq;
+   uint8_t dst_ioapic_id;
+   uint8_t dst_ioapic_intin;
+} __packed;
+typedef struct mpc_ioint_entry mpc_ioint_entry_t;
+
+struct mpc_lint_entry {
+   uint8_t type;
+   uint8_t int_type;
+   struct {
+       uint16_t po:1, el:1, rsvd:14;
+   };
+   uint8_t src_bus_id;
+   uint8_t src_bus_irq;
+   uint8_t dst_lapic_id;
+   uint8_t dst_lapic_lintin;
+} __packed;
+typedef struct mpc_lint_entry mpc_lint_entry_t;
+
 /* External declarations */
 
 extern void mptables_init(void);
--- smp/mptables.c
+++ smp/mptables.c
@@ -62,6 +62,29 @@ static mpf_t *get_mpf_addr(void) {
     return find_mpf(paddr_to_virt_kern(BIOS_ROM_ADDR_START), paddr_to_virt_kern(BIOS_ROM_ADDR_START + KB(64)));
 }
 
+static inline bool validate_mpc(mpc_hdr_t *ptr) {
+    if (ptr->signature != MPC_SIGNATURE)
+        return false;
+
+    if (ptr->spec_rev < 0x01 || ptr->spec_rev > 0x04)
+        return false;
+
+    if (get_mp_checksum(ptr, ptr->length) != 0x00)
+        return false;
+
+    return true;
+}
+
+static mpc_hdr_t *get_mpc_addr(const mpf_t *mpf_ptr) {
+    mpc_hdr_t *mpc_ptr;
+
+    mpc_ptr = paddr_to_virt_kern(mpf_ptr->mpc_base);
+    if (!validate_mpc(mpc_ptr))
+        panic("Incorrect MP Configuration Table found!\n");
+
+    return mpc_ptr;
+}
+
 static void dump_mpf(mpf_t *ptr) {
     printk("\nMP Floating Pointer Structure at %p:\n", ptr);
     printk("  Signature: %.4s\n", &ptr->signature);
@@ -74,8 +97,130 @@ static void dump_mpf(mpf_t *ptr) {
     printk("  Mode implemented: %s\n", ptr->imcrp ? "PIC Mode" : "Virtual Wire Mode");
 }
 
+static void dump_mpc_hdr(mpc_hdr_t *ptr) {
+    printk("\nMP Configuration Table Header at %p:\n", ptr);
+    printk("  Signature: %.4s\n", &ptr->signature);
+    printk("  Length: 0x%02x\n", ptr->length);
+    printk("  Spec revision: 0x%02x\n", ptr->spec_rev);
+    printk("  Checksum: 0x%02x\n", ptr->checksum);
+    printk("  OEM ID: %.*s\n", sizeof(ptr->oem_id), ptr->oem_id);
+    printk("  Product ID: %.*s\n", sizeof(ptr->product_id), ptr->product_id);
+    printk("  OEM Table Ptr: %p\n", ptr->oem_tlb_ptr);
+    printk("  OEM Table Size: %x\n", ptr->oem_tlb_size);
+    printk("  Entry count: %p\n", ptr->entry_count);
+    printk("  LAPIC base address: %p\n", ptr->lapic_base);
+    printk("  Extended Table Length: %x\n", ptr->ext_length);
+    printk("  Extended Table Checksum: %x\n", ptr->ext_checksum);
+}
+
+static inline void dump_mpc_processor_entry(const mpc_processor_entry_t *e) {
+    printk("  CPU: LAPIC ID=0x%02x, LAPIC version=0x%02x, Enabled=%x,"
+           " BSP=%x, CPU F/M/S: 0x%02x/0x%02x/0x%02x, Features=0x%08x\n",
+           e->lapic_id, e->lapic_version, e->en, e->bsp, e->family,
+           e->model, e->stepping, e->feature_flags);
+}
+
+static inline void dump_mpc_bus_entry(const mpc_bus_entry_t *e) {
+    printk("  BUS: ID=0x%02x Type=%.6s\n", e->id , e->type_str);
+}
+
+static inline void dump_mpc_ioapic_entry(const mpc_ioapic_entry_t *e) {
+    printk("  IOAPIC: ID=%x, Version=%x, Enabled=%x, Address: %p\n",
+           e->id, e->version, e->en, e->base_addr);
+}
+
+static const char *mpc_interrupt_type_names[] = {
+    [MPC_IOINT_INT]    = "INT",
+    [MPC_IOINT_NMI]    = "NMI",
+    [MPC_IOINT_SMI]    = "SMI",
+    [MPC_IOINT_EXTINT] = "ExtINT",
+};
+
+static const char *mpc_interrupt_polarity_names[] = {
+    [MPC_IOINT_POLARITY_BS]   = "Bus Spec",
+    [MPC_IOINT_POLARITY_AH]   = "Active High",
+    [MPC_IOINT_POLARITY_RSVD] = "Reserved",
+    [MPC_IOINT_POLARITY_AL]   = "Active Low",
+};
+
+static const char *mpc_interrupt_trigger_names[] = {
+    [MPC_IOINT_TRIGGER_BS]   = "Bus Spec",
+    [MPC_IOINT_TRIGGER_ET]   = "Edge",
+    [MPC_IOINT_TRIGGER_RSVD] = "Reserved",
+    [MPC_IOINT_TRIGGER_LT]   = "Level",
+};
+
+static inline void dump_mpc_ioint_entry(const mpc_ioint_entry_t *e) {
+    printk("     IO Int: Type=%6s, Polarity=%11s, Trigger=%9s, Source Bus: "
+           "ID=0x%02x IRQ=0x%02x, Dest IOAPIC: ID=0x%02x,  INTIN#=0x%02x\n",
+           mpc_interrupt_type_names[e->int_type],
+           mpc_interrupt_polarity_names[e->po],
+           mpc_interrupt_trigger_names[e->el],
+           e->src_bus_id, e->src_bus_irq,
+           e->dst_ioapic_id, e->dst_ioapic_intin);
+}
+
+static inline void dump_mpc_lint_entry(const mpc_lint_entry_t *e) {
+    printk("  Local Int: Type=%6s, Polarity=%11s, Trigger=%9s, Source Bus: "
+           "ID=0x%02x IRQ=0x%02x, Dest  LAPIC: ID=0x%02x, LINTIN#=0x%02x\n",
+           mpc_interrupt_type_names[e->int_type],
+           mpc_interrupt_polarity_names[e->po],
+           mpc_interrupt_trigger_names[e->el],
+           e->src_bus_id, e->src_bus_irq,
+           e->dst_lapic_id, e->dst_lapic_lintin);
+}
+
+static void process_mpc_entries(mpc_hdr_t *mpc_ptr) {
+    uint8_t *entry_ptr = (uint8_t *) (mpc_ptr + 1);
+
+    printk("\nMP Configuration Table Entries at %p:\n", entry_ptr);
+
+    for (int i = 0; i < mpc_ptr->entry_count; i++) {
+        switch (*entry_ptr) {
+        case MPC_PROCESSOR_ENTRY: {
+            mpc_processor_entry_t *mpc_cpu = (mpc_processor_entry_t *) entry_ptr;
+
+            dump_mpc_processor_entry(mpc_cpu);
+            entry_ptr += sizeof(*mpc_cpu);
+            break;
+        }
+        case MPC_BUS_ENTRY: {
+            mpc_bus_entry_t *mpc_bus = (mpc_bus_entry_t *) entry_ptr;
+
+            dump_mpc_bus_entry(mpc_bus);
+            entry_ptr += sizeof(*mpc_bus);
+            break;
+        }
+        case MPC_IOAPIC_ENTRY: {
+            mpc_ioapic_entry_t *mpc_ioapic = (mpc_ioapic_entry_t *) entry_ptr;
+
+            dump_mpc_ioapic_entry(mpc_ioapic);
+            entry_ptr += sizeof(*mpc_ioapic);
+            break;
+        }
+        case MPC_IO_INT_ENTRY: {
+            mpc_ioint_entry_t *mpc_ioint = (mpc_ioint_entry_t *) entry_ptr;
+
+            dump_mpc_ioint_entry(mpc_ioint);
+            entry_ptr += sizeof(*mpc_ioint);
+            break;
+        }
+        case MPC_LOCAL_INT_ENTRY: {
+            mpc_lint_entry_t *mpc_lint = (mpc_lint_entry_t *) entry_ptr;
+
+            dump_mpc_lint_entry(mpc_lint);
+            entry_ptr += sizeof(*mpc_lint);
+            break;
+        }
+        default:
+            panic("Unknown MP Configuration Table entry type: %x\n", *entry_ptr);
+        }
+    }
+}
+
 void mptables_init(void) {
     mpf_t *mpf_ptr = get_mpf_addr();
+    mpc_hdr_t *mpc_ptr;
 
     if (!mpf_ptr) {
         printk("No MP Floating Structure Pointer found!\n");
@@ -83,4 +228,11 @@ void mptables_init(void) {
     }
 
     dump_mpf(mpf_ptr);
+
+    if (mpf_ptr->mpc_type > 0 || mpf_ptr->mpc_base == 0x0)
+        panic("No MP Configuration Table present!\n");
+
+    mpc_ptr = get_mpc_addr(mpf_ptr);
+    dump_mpc_hdr(mpc_ptr);
+    process_mpc_entries(mpc_ptr);
 }
