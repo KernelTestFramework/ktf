6ceb61a5f26af3aabe94248ea26d52038a5bb3ed
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-05-23 09:16:56 +0200

    setup: unify and simplify addr_ranges[] handling
    Create a single array for all region types. Distinguish between various
    types by .base. Terminate the array with a NULL field.
    Add for_each_memory_range() macro to easy looping.
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

--- setup.c
+++ setup.c
@@ -30,41 +30,38 @@ uint8_t user_stack[PAGE_SIZE] __aligned(PAGE_SIZE) __user_data;
 
 char kernel_cmdline[PAGE_SIZE];
 
-addr_range_t kern_addr_ranges[] = {
-    { .name = ".text",   .base = VIRT_KERNEL_BASE, .flags = L1_PROT_RO, .from = __start_text,         .to = __end_text         },
-    { .name = ".data",   .base = VIRT_KERNEL_BASE, .flags = L1_PROT,    .from = __start_data,         .to = __end_data         },
-    { .name = ".bss",    .base = VIRT_KERNEL_BASE, .flags = L1_PROT,    .from = __start_bss,          .to = __end_bss          },
-    { .name = ".rodata", .base = VIRT_KERNEL_BASE, .flags = L1_PROT,    .from = __start_rodata,       .to = __end_rodata       },
-    { .name = "VIDEO",   .base = VIRT_KERNEL_BASE, .flags = L1_PROT,    .from = _ptr(VIRT_KERNEL_BASE + VGA_START_ADDR), .to = _ptr(VIRT_KERNEL_BASE + VGA_END_ADDR) },
-};
+#define VIDEO_START _ptr(VIRT_KERNEL_BASE + VGA_START_ADDR)
+#define VIDEO_END  _ptr(VIRT_KERNEL_BASE + VGA_END_ADDR)
+addr_range_t addr_ranges[] = {
+    { .name = "Low memory", .base = VIRT_IDENT_BASE, .flags = L1_PROT_RO,      .from = _ptr(0x0),         .to = _ptr(MB(1))     },
+    { .name = ".text.init", .base = VIRT_IDENT_BASE, .flags = L1_PROT_RO,      .from = __start_text_init, .to = __end_text_init },
+    { .name = ".data.init", .base = VIRT_IDENT_BASE, .flags = L1_PROT,         .from = __start_data_init, .to = __end_data_init },
+    { .name = ".bss.init",  .base = VIRT_IDENT_BASE, .flags = L1_PROT,         .from = __start_bss_init,  .to = __end_bss_init  },
 
-addr_range_t user_addr_ranges[] = {
-    { .name = ".text.user", .base = VIRT_USER_BASE, .flags = L1_PROT_USER_RO, .from = __start_text_user, .to = __end_text_user },
-    { .name = ".data.user", .base = VIRT_USER_BASE, .flags = L1_PROT_USER,    .from = __start_data_user, .to = __end_data_user },
-    { .name = ".bss.user",  .base = VIRT_USER_BASE, .flags = L1_PROT_USER,    .from = __start_bss_user,  .to = __end_bss_user  },
-};
+    { .name = ".rmode",     .base = VIRT_IDENT_BASE, .flags = L1_PROT,         .from = __start_rmode,  .to = __end_rmode        },
 
-addr_range_t init_addr_ranges[] = {
-    { .name = "Low memory", .base = VIRT_IDENT_BASE, .flags = L1_PROT, .from = _ptr(0x0),         .to = _ptr(MB(1))     },
-    { .name = ".rmode",  .base = VIRT_IDENT_BASE, .flags = L1_PROT,    .from = __start_rmode,  .to = __end_rmode  },
-    { .name = ".text.init", .base = VIRT_IDENT_BASE, .flags = L1_PROT_RO, .from = __start_text_init, .to = __end_text_init },
-    { .name = ".data.init", .base = VIRT_IDENT_BASE, .flags = L1_PROT,    .from = __start_data_init, .to = __end_data_init },
-    { .name = ".bss.init",  .base = VIRT_IDENT_BASE, .flags = L1_PROT,    .from = __start_bss_init,  .to = __end_bss_init  },
-};
+    { .name = ".text.user", .base = VIRT_USER_BASE,  .flags = L1_PROT_USER_RO, .from = __start_text_user, .to = __end_text_user },
+    { .name = ".data.user", .base = VIRT_USER_BASE,  .flags = L1_PROT_USER,    .from = __start_data_user, .to = __end_data_user },
+    { .name = ".bss.user",  .base = VIRT_USER_BASE,  .flags = L1_PROT_USER,    .from = __start_bss_user,  .to = __end_bss_user  },
 
-static inline void display_addr_range(addr_range_t *r) {
-    printk("%10s: VA: [0x%016lx - 0x%016lx] PA: [0x%08lx - 0x%08lx]\n",
-           r->name, r->from, r->to, r->from - r->base, r->to - r->base);
-}
+    { .name = ".text",   .base = VIRT_KERNEL_BASE,   .flags = L1_PROT_RO,      .from = __start_text,      .to = __end_text      },
+    { .name = ".data",   .base = VIRT_KERNEL_BASE,   .flags = L1_PROT,         .from = __start_data,      .to = __end_data      },
+    { .name = ".bss",    .base = VIRT_KERNEL_BASE,   .flags = L1_PROT,         .from = __start_bss,       .to = __end_bss       },
+    { .name = ".rodata", .base = VIRT_KERNEL_BASE,   .flags = L1_PROT,         .from = __start_rodata,    .to = __end_rodata    },
+
+    { .name = "VIDEO",   .base = VIRT_KERNEL_BASE,   .flags = L1_PROT,         .from = VIDEO_START,       .to = VIDEO_END       },
+
+    { 0x0 } /* NULL array terminator */
+};
 
 void display_memory_map(void) {
     printk("Memory Map:\n");
 
-    for (int i = 0; i < ARRAY_SIZE(kern_addr_ranges); i++)
-        display_addr_range(&kern_addr_ranges[i]);
+    for_each_memory_range(r) {
+        printk("%11s: VA: [0x%016lx - 0x%016lx] PA: [0x%08lx - 0x%08lx]\n",
+               r->name, r->from, r->to, r->from - r->base, r->to - r->base);
+    }
 
-    for (int i = 0; i < ARRAY_SIZE(user_addr_ranges); i++)
-        display_addr_range(&user_addr_ranges[i]);
 }
 
 static void init_console(void) {
