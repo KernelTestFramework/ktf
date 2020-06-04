55a735642e9077d81888007ef507721cb07089cf
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-06-04 12:33:14 +0200

    pmm: move physical memory configuration to PMM
    Add new files mm/pmm.c and include/mm/pmm.h, where all address ranges
    and physical memory management functionality is stored.
    
    Move the existing functionality from setup.{c,h} to pmm.{c,h}.
    
    Simplify and align addr_ranges[] array to make it more readible.
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

--- setup.c
+++ setup.c
@@ -9,10 +9,10 @@
 #include <pagetable.h>
 #include <multiboot.h>
 
+#include <mm/pmm.h>
 #include <smp/smp.h>
 
 #include <drivers/serial.h>
-#include <drivers/vga.h>
 
 bool opt_debug;
 
@@ -29,39 +29,6 @@ uint8_t kernel_stack[7 * PAGE_SIZE] __aligned(PAGE_SIZE) __data;
 uint8_t user_stack[PAGE_SIZE] __aligned(PAGE_SIZE) __user_data;
 
 const char *kernel_cmdline;
-#define VIDEO_START _ptr(VIRT_KERNEL_BASE + VGA_START_ADDR)
-#define VIDEO_END  _ptr(VIRT_KERNEL_BASE + VGA_END_ADDR)
-addr_range_t addr_ranges[] = {
-    { .name = "Low memory", .base = VIRT_IDENT_BASE, .flags = L1_PROT_RO,      .from = _ptr(0x0),         .to = _ptr(MB(1))     },
-    { .name = ".text.init", .base = VIRT_IDENT_BASE, .flags = L1_PROT_RO,      .from = __start_text_init, .to = __end_text_init },
-    { .name = ".data.init", .base = VIRT_IDENT_BASE, .flags = L1_PROT,         .from = __start_data_init, .to = __end_data_init },
-    { .name = ".bss.init",  .base = VIRT_IDENT_BASE, .flags = L1_PROT,         .from = __start_bss_init,  .to = __end_bss_init  },
-
-    { .name = ".rmode",     .base = VIRT_IDENT_BASE, .flags = L1_PROT,         .from = __start_rmode,  .to = __end_rmode        },
-
-    { .name = ".text.user", .base = VIRT_USER_BASE,  .flags = L1_PROT_USER_RO, .from = __start_text_user, .to = __end_text_user },
-    { .name = ".data.user", .base = VIRT_USER_BASE,  .flags = L1_PROT_USER,    .from = __start_data_user, .to = __end_data_user },
-    { .name = ".bss.user",  .base = VIRT_USER_BASE,  .flags = L1_PROT_USER,    .from = __start_bss_user,  .to = __end_bss_user  },
-
-    { .name = ".text",   .base = VIRT_KERNEL_BASE,   .flags = L1_PROT_RO,      .from = __start_text,      .to = __end_text      },
-    { .name = ".data",   .base = VIRT_KERNEL_BASE,   .flags = L1_PROT,         .from = __start_data,      .to = __end_data      },
-    { .name = ".bss",    .base = VIRT_KERNEL_BASE,   .flags = L1_PROT,         .from = __start_bss,       .to = __end_bss       },
-    { .name = ".rodata", .base = VIRT_KERNEL_BASE,   .flags = L1_PROT,         .from = __start_rodata,    .to = __end_rodata    },
-
-    { .name = "VIDEO",   .base = VIRT_KERNEL_BASE,   .flags = L1_PROT,         .from = VIDEO_START,       .to = VIDEO_END       },
-
-    { 0x0 } /* NULL array terminator */
-};
-
-void display_memory_map(void) {
-    printk("Memory Map:\n");
-
-    for_each_memory_range(r) {
-        printk("%11s: VA: [0x%016lx - 0x%016lx] PA: [0x%08lx - 0x%08lx]\n",
-               r->name, r->from, r->to, r->from - r->base, r->to - r->base);
-    }
-
-}
 
 static void init_console(void) {
     get_com_ports();
