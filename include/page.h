35fa080ad51a2cc5a2789309d326b93c7363494e
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-07-23 08:42:48 +0000

    pmm: properly detect invalid paddr and mfn
    Do not assume that only invalid physical addresses or frames are
    only these of 0x0. To be able to correctly check if paddr or mfn
    is invalid, go through all available physical memory regions and
    check if paddr or mfn belongs to it. When found a matching region
    the paddr/mfn is valid, otherwise invalid.
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

--- include/page.h
+++ include/page.h
@@ -104,9 +104,6 @@ typedef unsigned long mfn_t;
 #define PADDR_INVALID (0UL)
 #define MFN_INVALID (0UL)
 
-static inline bool paddr_invalid(paddr_t pa) { return pa == PADDR_INVALID; }
-static inline bool mfn_invalid(mfn_t mfn) { return mfn == MFN_INVALID; }
-
 static inline mfn_t paddr_to_mfn(paddr_t pa)  { return (mfn_t) (pa >> PAGE_SHIFT);    }
 static inline paddr_t mfn_to_paddr(mfn_t mfn) { return (paddr_t) (mfn << PAGE_SHIFT); }
 
