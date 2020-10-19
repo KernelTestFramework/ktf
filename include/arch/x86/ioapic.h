252be82e597adc2b7b61a761122c6b0a2daab97b
Pawel Wieczorkiewicz <wipawel@amazon.de>
2020-10-19 15:56:08 +0200

    ioapic: add irq overrides detection and handling
    Detect ACPI and MP tables provided IRQ and NMI overrides for IOAPIC
    and LAPIC.  Store them on their bus's overrides list.  Add interface
    functions to obtain ISA and PCI bus IRQ overrides.
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

--- include/arch/x86/ioapic.h
+++ include/arch/x86/ioapic.h
@@ -29,7 +29,36 @@
 #include <lib.h>
 #include <list.h>
 
+#define IOAPIC_DEST_ID_UNKNOWN ((uint8_t) 0xFF)
+
 #ifndef __ASSEMBLY__
+
+enum irq_override_polarity {
+    IOAPIC_IRQ_OVR_POLARITY_BS = 0x00,
+    IOAPIC_IRQ_OVR_POLARITY_AH = 0x01,
+    IOAPIC_IRQ_OVR_POLARITY_RSVD = 0x02,
+    IOAPIC_IRQ_OVR_POLARITY_AL = 0x03,
+};
+typedef enum irq_override_polarity irq_override_polarity_t;
+
+enum irq_override_trigger_mode {
+    IOAPIC_IRQ_OVR_TRIGGER_BS = 0x00,
+    IOAPIC_IRQ_OVR_TRIGGER_ET = 0x01,
+    IOAPIC_IRQ_OVR_TRIGGER_RSVD = 0x02,
+    IOAPIC_IRQ_OVR_TRIGGER_LT = 0x03,
+};
+typedef enum irq_override_trigger_mode irq_override_trigger_mode_t;
+
+struct irq_override {
+    list_head_t list;
+    uint8_t type;
+    uint32_t src;
+    uint32_t dst;
+    uint8_t dst_id; /* IOAPIC or LAPIC */
+    irq_override_polarity_t polarity;
+    irq_override_trigger_mode_t trigger_mode;
+};
+typedef struct irq_override irq_override_t;
 
 struct bus {
     list_head_t list;
@@ -39,9 +68,60 @@ struct bus {
 };
 typedef struct bus bus_t;
 
+enum ioapic_irq_type {
+    IOAPIC_IRQ_TYPE_INT = 0x00,
+    IOAPIC_IRQ_TYPE_NMI = 0x01,
+    IOAPIC_IRQ_TYPE_SMI = 0x10,
+    IOAPIC_IRQ_TYPE_EXTINT = 0x11,
+};
+typedef enum ioapic_irq_type ioapic_irq_type_t;
+
+enum ioapic_delivery_mode {
+    IOAPIC_DELIVERY_MODE_FIXED = 0x00,
+    IOAPIC_DELIVERY_MODE_LOWPRIO = 0x01,
+    IOAPIC_DELIVERY_MODE_SMI = 0x02,
+    IOAPIC_DELIVERY_MODE_NMI = 0x04,
+    IOAPIC_DELIVERY_MODE_INIT = 0x05,
+    IOAPIC_DELIVERY_MODE_EXTINT = 0x07,
+};
+typedef enum ioapic_deliver_mode ioapic_delivery_mode_t;
+
+enum ioapic_dest_mode {
+    IOAPIC_DEST_MODE_PHYSICAL = 0x00,
+    IOAPIC_DEST_MODE_LOGICAL = 0x01,
+};
+typedef enum ioapic_dest_mode ioapic_dest_mode_t;
+
+enum ioapic_delivery_status {
+    IOAPIC_DELIVERY_STATUS_IDLE = 0x00,
+    IOAPIC_DELIVERY_STATUS_PENDING = 0x01,
+};
+typedef enum ioapic_delivery_status ioapic_delivery_status_t;
+
+enum ioapic_trigger_mode {
+    IOAPIC_TRIGGER_MODE_EDGE = 0x00,
+    IOAPIC_TRIGGER_MODE_LEVEL = 0x01,
+};
+typedef enum ioapic_trigger_mode ioapic_trigger_mode_t;
+
+enum ioapic_polarity {
+    IOAPIC_POLARITY_AH = 0x00,
+    IOAPIC_POLARITY_AL = 0x01,
+};
+typedef enum ioapic_polarity ioapic_polarity_t;
+
+enum ioapic_int_mask {
+    IOAPIC_INT_UNMASK = 0x00,
+    IOAPIC_INT_MASK = 0x01,
+};
+typedef enum ioapic_int_mask ioapic_int_mask_t;
+
 /* External declarations */
 
 extern bus_t *add_system_bus(uint8_t id, const char *name, size_t namelen);
+extern int add_system_bus_irq_override(uint8_t bus_id, irq_override_t *irq_override);
+extern irq_override_t *get_system_isa_bus_irq(uint8_t irq_type, uint32_t irq_src);
+extern irq_override_t *get_system_pci_bus_irq(uint8_t irq_type, uint32_t irq_src);
 
 #endif /* __ASSEMBLY__ */
 
