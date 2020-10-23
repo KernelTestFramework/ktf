30a0584c695c89640d6950a4b238c515d0433e5c
82marbag <69267416+82marbag@users.noreply.github.com>
2020-10-23 12:03:10 -0400

    keyboard: 8042 initialization
    Initialize the 8042 controller of the keyboard
    
    Signed-off-by: Daniele Ahmed <ahmeddan amazon c;om >

--- include/drivers/keyboard.h
+++ include/drivers/keyboard.h
@@ -30,16 +30,48 @@
 #define KEYBOARD_PORT_CMD  0x64 /* keyboard command port */
 #define KEYBOARD_PORT_DATA 0x60 /* keyboard data port */
 
-#define KEYBOARD_IRQ0_OFFSET (PIC_IRQ0_OFFSET + 1) /* keyboard irq offset */
-#define KEYBOARD_IRQ_UNMASK  (~2) /* mask with keyboard irq enabled, others disabled */
+#define KEYBOARD_IRQ   0x01
+#define KEYBOARD_IRQ_2 0x04 /* Second port */
+#define KEYBOARD_IRQ0_OFFSET                                                             \
+    (PIC_IRQ0_OFFSET + KEYBOARD_IRQ) /* keyboard first channel irq offset */
+#define KEYBOARD_2_IRQ0_OFFSET (PIC_IRQ0_OFFSET + KEYBOARD_IRQ_2)
 
-#define KEYBOARD_STATUS_OUT_FULL 1 /* bit set when the keyboard buffer is full */
+#define KEYBOARD_STATUS_OUT_FULL 0x01 /* bit set when the keyboard buffer is full */
+
+enum {
+    KEYBOARD_CMD_WRITE_CONFIGURATION = 0x60, /* Write configuration byte */
+    KEYBOARD_CMD_READ_CONFIGURATION = 0x20,  /* Read configuration byte */
+    KEYBOARD_CMD_SELF_TEST = 0xAA,
+    KEYBOARD_CMD_TEST_PORT_0 = 0xAB,
+    KEYBOARD_CMD_TEST_PORT_1 = 0xA9,
+    KEYBOARD_CMD_DISABLE_PORT_0 = 0xAD,
+    KEYBOARD_CMD_DISABLE_PORT_1 = 0xA7,
+    KEYBOARD_CMD_ENABLE_PORT_0 = 0xAE,
+    KEYBOARD_CMD_ENABLE_PORT_1 = 0xA8,
+} keyboard_cmd;
+typedef enum keyboard_cmd keyboard_cmd_t;
+
+enum {
+    KEYBOARD_RES_SELF_TEST = 0x55,
+} keyboard_res;
+
+enum {
+    KEYBOARD_CONTROLLER_CONFIG_BIT_PORT_0_INTERRUPT,
+    KEYBOARD_CONTROLLER_CONFIG_BIT_PORT_1_INTERRUPT,
+    KEYBOARD_CONTROLLER_CONFIG_BIT_SYS_FLAG,
+    KEYBOARD_CONTROLLER_CONFIG_BIT_ZERO,
+    KEYBOARD_CONTROLLER_CONFIG_BIT_CLOCK_0,
+    KEYBOARD_CONTROLLER_CONFIG_BIT_CLOCK_1,
+    KEYBOARD_CONTROLLER_CONFIG_BIT_TRANSLATION,
+    KEYBOARD_CONTROLLER_CONFIG_BIT_ZERO_,
+} keyboard_controller_config_byte;
+typedef enum keyboard_controller_config_byte keyboard_controller_config_byte_t;
 
 #define KEY_BUF 80 /* size of internal input buffer */
 
 #define SCAN_RELEASE_MASK 0x80 /* bit set on key release */
 
-#define KEY_NULL 0x0 /* no key */
+#define KEY_NULL 0x00 /* no key */
 
 /* scan codes */
 #define SCAN_NULL       0x0
@@ -76,7 +108,7 @@
 #define SCAN_INS        0x52
 #define SCAN_DEL        0x53
 
-void init_keyboard(void);
+void init_keyboard();
 void keyboard_interrupt_handler(void);
 unsigned int keyboard_process_keys(void);
 
