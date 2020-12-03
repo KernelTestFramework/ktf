0629e0b1f5d06295c37beda8b8a0962e3c5b678c
Deepak Gupta <dkgupta@amazon.com>
2020-12-03 01:30:28 -0800

    more libc definitions: adding more libc definitions
    Needed MAX_INT and PRIx64 for pfmlib library. Earlier they were coming
    from standard include headers from dev host.
    
    Signed-off-by: Deepak Gupta <dkgupta@amazon.com>

--- include/limits.h
+++ include/limits.h
@@ -3,3 +3,13 @@
 #define ULONG_MAX __UINT64_MAX__
 #define LONG_MAX  __INT64_MAX__
 #define LONG_MIN  (-LONG_MAX - 1L)
+
+/* Minimum and maximum values a `signed int' can hold.  */
+#undef INT_MAX
+#define INT_MAX __INT_MAX__
+#undef INT_MIN
+#define INT_MIN (-INT_MAX - 1)
+
+/* Maximum value an `unsigned int' can hold.  (Minimum is 0).  */
+#undef UINT_MAX
+#define UINT_MAX (INT_MAX * 2U + 1U)
