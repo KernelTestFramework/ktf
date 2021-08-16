a4099705536f32c8f6e2ab4d2b90978e5a6baa77
Pawel Wieczorkiewicz <wipawel@amazon.de>
2021-08-16 17:26:11 +0200

    pmm: find unused begining of each region during init
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@amazon.de>

--- mm/pmm.c
+++ mm/pmm.c
@@ -82,7 +82,11 @@ static size_t process_memory_range(unsigned index) {
     if (get_avail_memory_range(index, &range) < 0)
         return 0;
 
-    cur = start = (index == 1 ? virt_to_paddr(__end_rodata) : _paddr(range.start));
+    /* Find unused beginning of the region */
+    for (start = _paddr(range.start); !in_free_region(start); start += PAGE_SIZE)
+        ;
+
+    cur = start;
     end = _paddr(range.end);
     size = end - start;
 
