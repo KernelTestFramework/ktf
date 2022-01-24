2edcd156d265ca4e91110f4b773c8f6676b5fcd7
Pawel Wieczorkiewicz <wipawel@grsecurity.net>
2022-01-24 15:26:01 +0100

    cpuid: move functions from cpuid.h into cpuid.c
    While at it, move cpuid.h into arch/x86 directory.
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@grsecurity.net>

---
+++ include/arch/x86/cpuid.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2020 Amazon.com, Inc. or its affiliates.
+ * All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef KTF_CPUID_H
+#define KTF_CPUID_H
+
+#include <ktf.h>
+
+/* CPU vendor detection */
+#define CPUID_EXT_INFO_LEAF  0x80000000U
+#define CPUID_BRAND_INFO_MIN 0x80000002U
+#define CPUID_BRAND_INFO_MAX 0x80000004U
+
+extern uint64_t get_cpu_freq(const char *cpu_str);
+extern bool cpu_vendor_string(char *cpu_str);
+
+#endif /* KTF_CPUID_H */
