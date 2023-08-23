c351a82ed30d8bf5a6a8a96d21217a253b7f7c5e
Pawel Wieczorkiewicz <wipawel@grsecurity.net>
2023-08-23 14:10:41 +0200

    drivers,fb: make scrolling optional (default)
    Framebuffer scrolling can be enabled/disabled either on boot time
    via 'fb_scroll' cmdline parameter or on runtime using set_fb_scroll();
    
    Signed-off-by: Pawel Wieczorkiewicz <wipawel@grsecurity.net>

--- include/drivers/fb.h
+++ include/drivers/fb.h
@@ -36,5 +36,6 @@ extern void draw_line(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2, uint32
 extern void draw_logo(void);
 extern void fb_write(void *fb_addr, const char *buf, size_t len, uint32_t color);
 extern bool setup_framebuffer(void);
+extern void set_fb_scroll(bool state);
 
 #endif /* KTF_DRV_FB_H */
